57f30651beba6a0bd6816b1d589053ebfeb28626
anyharris <DT90uNt>
2021-02-01 15:40:48 +0700

    added USDT lending

--- bots.py
+++ bots.py
@@ -14,11 +14,15 @@ tg_bnb_bot_token = os.getenv('BNBBOTTOKEN')
 tg_bnb_channel_id = os.getenv('BNBCHANNEL')
 tg_btc_bot_token = os.getenv('BTCBOTTOKEN')
 tg_btc_channel_id = os.getenv('BTCCHANNEL')
+tg_usdt_bot_token = os.getenv('USDTBOTTOKEN')
+tg_usdt_channel_id = os.getenv('USDTCHANNEL')
 
 tg_eth = Telegram(tg_eth_bot_token, tg_eth_channel_id)
 tg_usd = Telegram(tg_usd_bot_token, tg_usd_channel_id)
 tg_bnb = Telegram(tg_bnb_bot_token, tg_bnb_channel_id)
 tg_btc = Telegram(tg_btc_bot_token, tg_btc_channel_id)
+tg_usdt = Telegram(tg_usdt_bot_token, tg_usdt_channel_id)
+
 ftx = Ftx(ftx_api_key, ftx_api_secret)
 
 response = ftx.get_spot_margin_borrow().json()
@@ -44,3 +48,7 @@ for ticker in rates:
         apy = "{:.1%}".format(ticker['previous'] * 24 * 365)
         message = f'BTC lending: {apy}'
         tg_btc.broadcast(message)
+    if ticker['coin'] == 'USDT':
+        apy = "{:.1%}".format(ticker['previous'] * 24 * 365)
+        message = f'USDT lending: {apy}'
+        tg_usdt.broadcast(message)
